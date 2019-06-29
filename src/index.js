9416c2567dd62de37f934fb1674dfb850d692889
Timur Bilotserkivky <timur.belotserkovsky@gmail.com>
2019-06-29 18:37:26 +0300

    first commit, login to VRM API

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// const request = require('request');
+// import * as api from "./api";
+
+const request = require('request');
+const login_pass = require('./login_pass.js');
+
+
+
+//APIs:
+// https://vrmapi.victronenergy.com/v2/auth/login
+const url = "https://vrmapi.victronenergy.com";
+const auth = "/v2/auth/login";
+
+//This will be asked
+let username = login_pass.username;
+let password = login_pass.password;
+console.log(username, password);
+
+login(username, password);
+
+
+
+function login(login, password) {
+    request('https://vrmapi.victronenergy.com/v2/auth/login', {method: "GET", body: `{"username": "${username}", "password": "${password}"}`}, (err, res, body) => {
+        console.log(res.statusCode);
+
+        if (res.statusCode == 200) {
+            let user_token = JSON.parse(body);
+            let token = "";
+            let userId = null;
+            token = user_token.token;
+            uderId = user_token.idUser;
+            console.log(user_token);
+            return {
+                userId,
+                token
+            };
+        }
+    });
+};
+
+// async function toLogin(){
+//     let user_token = await login(username, password);
+//     console.log(user_token);
+// };
+
+// toLogin();
+
+// console.log(user_token);
+
+
