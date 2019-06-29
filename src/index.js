92c7788371e6cd7aa3550fa4652f992fcd0fdd60
Timur Bilotserkivky <timur.belotserkovsky@gmail.com>
2019-06-30 00:08:29 +0300

    add callback to whait token responce

--- src/index.js
+++ src/index.js
@@ -14,13 +14,12 @@ const auth = "/v2/auth/login";
 //This will be asked
 let username = login_pass.username;
 let password = login_pass.password;
-console.log(username, password);
 
-login(username, password);
-
+//login(username, password);
 
 
-function login(login, password) {
+
+function login(login, password, callback) {
     request('https://vrmapi.victronenergy.com/v2/auth/login', {method: "GET", body: `{"username": "${username}", "password": "${password}"}`}, (err, res, body) => {
         console.log(res.statusCode);
 
@@ -29,15 +28,20 @@ function login(login, password) {
             let token = "";
             let userId = null;
             token = user_token.token;
-            uderId = user_token.idUser;
-            console.log(user_token);
-            return {
-                userId,
-                token
-            };
+            userId = user_token.idUser;
+            // console.log(user_token);
+            // return {
+            //     userId,
+            //     token
+            // };
+            callback(userId, token);
         }
     });
+
+   
 };
+
+login(username, password, (userId, token) => console.log(userId, token));
 
 // async function toLogin(){
 //     let user_token = await login(username, password);
