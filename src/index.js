2cb4d1b6dc53ebd3158269c98b702280c4eb393b
Timur Bilotserkivky <timur.belotserkovsky@gmail.com>
2019-06-30 23:01:31 +0300

    add promise instead of callback, add getinstallations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// const request = require('request');
-// import * as api from "./api";
+
 
 const request = require('request');
 const login_pass = require('./login_pass.js');
@@ -8,48 +7,95 @@ const login_pass = require('./login_pass.js');
 
 //APIs:
 // https://vrmapi.victronenergy.com/v2/auth/login
+// /v2/users/{idUser}/installations
+
 const url = "https://vrmapi.victronenergy.com";
-const auth = "/v2/auth/login";
+const authApi = "/v2/auth/login";
+
 
 //This will be asked
 let username = login_pass.username;
 let password = login_pass.password;
 
+let idUser;
+let loginStatus = false;
+const headerWithToken = {};
+
 //login(username, password);
-
-
-
-function login(login, password, callback) {
-    request('https://vrmapi.victronenergy.com/v2/auth/login', {method: "GET", body: `{"username": "${username}", "password": "${password}"}`}, (err, res, body) => {
-        console.log(res.statusCode);
-
-        if (res.statusCode == 200) {
-            let user_token = JSON.parse(body);
-            let token = "";
-            let userId = null;
-            token = user_token.token;
-            userId = user_token.idUser;
-            // console.log(user_token);
-            // return {
-            //     userId,
-            //     token
-            // };
-            callback(userId, token);
-        }
-    });
-
-   
-};
-
-login(username, password, (userId, token) => console.log(userId, token));
-
-// async function toLogin(){
-//     let user_token = await login(username, password);
-//     console.log(user_token);
+// function login(login, password, callback) {
+//     request(url + authApi, {method: "GET", body: `{"username": "${username}", "password": "${password}"}`}, (err, res, body) => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         if (res.statusCode == 200) {
+//             let user_token = JSON.parse(body);
+//             let token = user_token.token;
+//             let userId = user_token.idUser;
+//             loginStatus = true;
+//             idUser = userId; 
+            
+//             callback(userId, token);
+//         }
+//     }); 
 // };
 
-// toLogin();
 
-// console.log(user_token);
+// login(username, password, (userId, token) => {
+//     console.log(userId, token);
+//     headerWithToken['X-Authorization'] = 'Bearer ' + token;
+//     getInstallations(idUser);
+
+    //here we do wisible buttons of receiving other information
+
+// });
+
+function login(login, password) {
+    return new Promise((resolve, reject) => {
+        request(url + authApi, {method: "GET", body: `{"username": "${username}", "password": "${password}"}`}, (err, res, body) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            if (res.statusCode == 200) {
+                let user_token = JSON.parse(body);
+                idUser = user_token.idUser;
+                loginStatus = true;
+                resolve(user_token);
+            }
+        });
+    });
+     
+};
+
+
+login(username, password).then((user_token, token) => {
+    console.log(user_token);
+    headerWithToken['X-Authorization'] = 'Bearer ' + user_token.token;
+    getInstallations(idUser);
+});
+
+
+console.log('timing test');
+
+function getInstallations(idUser){
+    const installationsApi = `/v2/users/${idUser}/installations`;
+
+    request(url + installationsApi, {method:'GET', headers: headerWithToken}, (err, res, body) => {
+        if (err){
+            console.log(err);
+            return;
+        }
+        if (res.statusCode == 200){
+            let message = JSON.parse(body);
+            if (message.success == true){
+                message.records.forEach((elem) => console.log(elem.idSite + " : " + elem.name));
+                //console.log(message);
+            }
+        }
+    });
+};
+
+
 
 
